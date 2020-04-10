7f2cd41d8e3ba7bff5df6cb1c39398b765f9765d
Kamil Kolasa <kamilkolasa19@gmail.com>
2020-04-10 18:30:13 +0200

    ADD InitOrderForm and ADD processOrder

--- src/js/script.js
+++ src/js/script.js
@@ -58,7 +58,10 @@
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.processOrder();
+      thisProduct.initOrderForm();
       console.log('New Product: ', thisProduct);
     }
     renderInMenu(){
@@ -77,6 +80,17 @@
       menuContainer.appendChild(thisProduct.element);
     }
 
+    getElements(){
+      const thisProduct = this;
+    
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      console.log('Szukane produkty:',  thisProduct);
+    }
+
 
 
     initAccordion(){
@@ -87,7 +101,7 @@
       console.log('clickableTrigger: ', clickableTrigger);
 
       /* START: add click event listener to trigger */
-      clickableTrigger.addEventListener('click', function(){ 
+      thisProduct.accordionTrigger.addEventListener('click', function(){ 
 
         /* prevent default action for event */
         event.preventDefault(); 
@@ -111,6 +125,84 @@
           }
         }
       });
+    }
+
+    initOrderForm(){
+      const thisProduct = this;
+      //console.log(thisProduct.initOrderForm);
+
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+      
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+      
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+    processOrder(){
+      const thisProduct = this;
+      console.log(thisProduct);
+    
+      /* read all data from the form (using utils.serializeFormToObject) and save it to const formData */
+      const formData = utils.serializeFormToObject(thisProduct.form); // Pobranie danych z formularza i przypisanie do formData
+      console.log('formData ', formData);
+    
+      /* set variable price to equal thisProduct.data.price */
+      let price = thisProduct.data.price; // Przypisanie wartości thisProduct.data.price do zmiennej price
+      console.log('Price: ', price);
+
+      const params = thisProduct.data.params; // Przypisanie wartości thisProduct.data.params do zmiennej params
+      console.log('Params: ', params);
+    
+      /* START LOOP: for each paramId in thisProduct.data.params */
+      for (let paramId in params){ // Pętla - szukamy paramId we wszystkich params (thisProduct.data.params)
+        console.log('ParamID: ', paramId);
+        /* save the element in thisProduct.data.params with key paramId as const param */
+
+        const param = params[paramId]; // Przypisanie wartości params[paramId] do zmiennej param
+        console.log('Param: ', param);
+    
+        /* START LOOP: for each optionId in param.options */
+        for (let optionId in param.options){ // Pętla - szukamy optionId we wszystkich param.options
+
+          /* save the element in param.options with key optionId as const option */
+          const option = param.options[optionId]; // Przypisanie wartości param.options[optionId] do zmiennej option
+          console.log('OptionID is: ', option);
+
+          const optionSelected = formData.hasOwnProperty(paramId) && formData[paramId].indexOf(optionId) > -1; // Czy "-1" odnosi się do tablicy?
+          // W stałej optionSelected sprawdzamy czy istnieje formData[paramId], a jeśli tak, to czy ta tablica zawiera klucz równy wartości optionId
+
+          /* START IF: if option is selected and option is not default */
+          if (optionSelected && !option.default) { // Jeżeli optionSelected jest wybrany, a jednocześnie option.default nie jest domyślna to:
+
+            /* add price of option to variable price */
+            price = price + option.price;
+
+          /* END IF: if option is selected and option is not default */
+          }
+          /* START ELSE IF: if option is not selected and option is default */
+          else if (!optionSelected && option.default){ // Jeżeli optionSelected jest niewybrany, a jednocześnie option.default jest domyślna to:
+
+            /* deduct price of option from price */
+            price = price - option.price;
+
+          /* END ELSE IF: if option is not selected and option is default */
+          }
+        /* END LOOP: for each optionId in param.options */
+        }
+      /* END LOOP: for each paramId in thisProduct.data.params */
+      }
+      /* set the contents of thisProduct.priceElem to be the value of variable price */
+
+      thisProduct.priceElem.innerHTML = price; // Umieszczenie price w HTML, aby zmieniała się na stronie
     }
   }
 
