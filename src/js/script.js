712bf03599c20bc106a3b222c5ddeef63a795c0c
Kamil Kolasa <kamilkolasa19@gmail.com>
2020-04-12 15:01:34 +0200

    ADD Elements of widget

--- src/js/script.js
+++ src/js/script.js
@@ -231,9 +231,20 @@
   class AmountWidget{
     constructor(element){
       const thisWidget = this;
-
+      thisWidget.getElements(element);
+      
       console.log('AmountWidget: ', thisWidget);
       console.log('Constructor arguments: ', element);
+
+    }
+
+    getElements(element){
+      const thisWidget = this;
+      
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
     }
   }
 
