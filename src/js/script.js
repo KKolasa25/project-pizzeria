ae6c444aa276a6c960e516080c6bef3cb9937aed
Kamil Kolasa <kamilkolasa19@gmail.com>
2020-04-12 17:59:46 +0200

    ADD comments

--- src/js/script.js
+++ src/js/script.js
@@ -62,7 +62,7 @@
       thisProduct.getElements();
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
-      thisProduct.initAmountWidget();
+      thisProduct.initAmountWidget(); // wywołanie metody
       thisProduct.processOrder();
       console.log('New Product: ', thisProduct);
     }
@@ -220,17 +220,17 @@
       /* set the contents of thisProduct.priceElem to be the value of variable price */
 
       /* multiply price by amount */ 
-      price *= thisProduct.amountWidget.value; 
+      price *= thisProduct.amountWidget.value; // cena raz wybrana ilość w widgecie 
       thisProduct.priceElem.innerHTML = price; // Umieszczenie price w HTML, aby zmieniała się na stronie
     }
 
-    initAmountWidget(){
+    initAmountWidget(){ // metoda tworząca instancje klasy AmountWidget i zapisywała ją we właściwosci produktu
       const thisProduct = this;
 
-      thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+      thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem); // nowa instancja
 
-      thisProduct.amountWidgetElem.addEventListener('updated', function(){
-        thisProduct.processOrder();
+      thisProduct.amountWidgetElem.addEventListener('updated', function(){ // nasłuchiwanie eventu "updated"
+        thisProduct.processOrder(); // wywołanie metody
       });
     }
   }
@@ -247,7 +247,7 @@
       console.log('Constructor arguments: ', element);
     }
 
-    getElements(element){
+    getElements(element){ // zapisanie we właściwościach wszystkich elementów DOM które są nam potrzebne
       const thisWidget = this;
       
       thisWidget.element = element;
@@ -256,26 +256,27 @@
       thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
     }
 
-    setValue(value){
+    setValue(value){ // ustawianie nowej wartości widgetu
       const thisWidget = this;
 
-      const newValue = parseInt(value);
+      const newValue = parseInt(value); // parseInt przetwarza argument w postaci łańcucha znaków i zwraca liczbę całkowitą typu integer (przy wpisaniu słowa wyświetli się NaN)
 
       /* TODO: Add validation */
 
+      // jezeli nowa wartość jest inna niż wartośći domyślna a jednoczesnie nowa wartośćjest wieksza lub równa 1 i jednoczesnie nowa wartość jest mniejsza lub rowna 9 to:
       if (newValue != thisWidget.value && newValue >= settings.amountWidget.defaultMin && thisWidget.value && newValue <= settings.amountWidget.defaultMax ) {
         thisWidget.value = newValue; 
-        thisWidget.announce();
+        thisWidget.announce(); // wywołanie metody 
       }
 
-      thisWidget.input.value = thisWidget.value;
+      thisWidget.input.value = thisWidget.value; // wyświetlana wartość
     }
 
     initActions(){
       const thisWidget = this;
       
       thisWidget.input.addEventListener('change', function(){
-        thisWidget.setValue(thisWidget.input.value);
+        thisWidget.setValue(thisWidget.input.value); // "change" jest uruchamiany dla elementu input gdy jest on zmieniany (zmiana wartości inputu)
       });
 
       thisWidget.linkDecrease.addEventListener('click', function(event){
@@ -289,11 +290,11 @@
       });
     }
 
-    announce(){
+    announce(){ // metoda ktora tworzy instancje klasy Event 
       const thisWidget = this;
 
-      const event = new Event('updated');
-      thisWidget.element.dispatchEvent(event);
+      const event = new Event('updated'); // tworzenie nowego eventu 
+      thisWidget.element.dispatchEvent(event); // wywołanie eventu na kontenerze naszego widgetu / Wywołuje zdarzenie w bieżącym elemencie
     }
   }
 
