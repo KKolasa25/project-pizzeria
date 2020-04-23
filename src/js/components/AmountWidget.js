a716cf4867299b2cd60125134f7955d25cc22450
Kamil Kolasa <kamilkolasa19@gmail.com>
2020-04-23 22:07:41 +0200

    ADD BaseWidget.js, CHANGE in AmountWidget.js and Booking.js

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -1,68 +1,55 @@
-import {select, settings} from '../settings.js';
+import {settings, select} from '../settings.js';
+import {BaseWidget} from './BaseWidget.js';
 
-export class AmountWidget{
-
-  constructor(element){
+export class AmountWidget extends BaseWidget {
+  constructor(wrapper){
+    //thisWidget.getElements(element);
+    super(wrapper, settings.amountWidget.defaultValue);
     const thisWidget = this;
-    thisWidget.getElements(element);
-    thisWidget.value = settings.amountWidget.defaultValue;
-    thisWidget.setValue(thisWidget.input.value);
+
+    //thisWidget.value = settings.amountWidget.defaultValue;
+    //thisWidget.setValue(thisWidget.input.value);
+    thisWidget.getElements();
     thisWidget.initActions();
 
     //console.log('AmountWidget: ', thisWidget);
     //console.log('Constructor arguments: ', element);
   }
 
-  getElements(element){ // zapisanie we właściwościach wszystkich elementów DOM które są nam potrzebne
+  getElements(){ // zapisanie we właściwościach wszystkich elementów DOM które są nam potrzebne
     const thisWidget = this;
     
-    thisWidget.element = element;
-    thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-    thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-    thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    //thisWidget.dom = element;
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.amount.input);
+    thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
+    thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
   }
 
-  setValue(value){ // ustawianie nowej wartości widgetu
-    const thisWidget = this;
-
-    const newValue = parseInt(value); // parseInt przetwarza argument w postaci łańcucha znaków i zwraca liczbę całkowitą typu integer (przy wpisaniu słowa wyświetli się NaN)
-
-    /* TODO: Add validation */
-
-    // jezeli nowa wartość jest inna niż wartośći domyślna a jednoczesnie nowa wartośćjest wieksza lub równa 1 i jednoczesnie nowa wartość jest mniejsza lub rowna 9 to:
-    if (newValue != thisWidget.value && newValue >= settings.amountWidget.defaultMin && thisWidget.value && newValue <= settings.amountWidget.defaultMax ) {
-      thisWidget.value = newValue; 
-      thisWidget.announce(); // wywołanie metody 
-    }
-
-    thisWidget.input.value = thisWidget.value; // wyświetlana wartość
+  isValid(newValue){
+    return !isNaN(newValue) && newValue >= settings.amountWidget.defaultMin && newValue <= settings.amountWidget.defaultMax;
   }
 
   initActions(){
     const thisWidget = this;
     
-    thisWidget.input.addEventListener('change', function(){
-      thisWidget.setValue(thisWidget.input.value); // "change" jest uruchamiany dla elementu input gdy jest on zmieniany (zmiana wartości inputu)
+    thisWidget.dom.input.addEventListener('change', function(){
+      thisWidget.value = thisWidget.dom.input.value; // "change" jest uruchamiany dla elementu input gdy jest on zmieniany (zmiana wartości inputu)
     });
 
-    thisWidget.linkDecrease.addEventListener('click', function(event){
+    thisWidget.dom.linkDecrease.addEventListener('click', function(event){
       event.preventDefault();
-      thisWidget.setValue(thisWidget.value - 1);
+      thisWidget.value = --thisWidget.dom.input.value;
     });
 
-    thisWidget.linkIncrease.addEventListener('click', function(event){
+    thisWidget.dom.linkIncrease.addEventListener('click', function(event){
       event.preventDefault();
-      thisWidget.setValue(thisWidget.value + 1);
+      thisWidget.value = ++thisWidget.dom.input.value;
     });
   }
 
-  announce(){ // metoda ktora tworzy instancje klasy Event. Jej zadaniem jest wywołanie eventu updated
+  renderValue(){
     const thisWidget = this;
 
-    const event = new CustomEvent ('updated', {
-      bubbles: true
-    }); 
-
-    thisWidget.element.dispatchEvent(event); // wywołanie eventu na kontenerze naszego widgetu / Wywołuje zdarzenie w bieżącym elemencie
+    thisWidget.dom.input.value = thisWidget.value;
   }
 }
